a64f973339a86d684c64581cdde2b0371b57fec9
lalocesar2003 <ceed1000@gmail.com>
2025-02-01 15:56:50 -0500

    1 hora con 40 minutos

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,5 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const page = () => {
-  return <div>page</div>;
-};
+const SignUp = () => <AuthForm type="sign-up" />;
 
-export default page;
+export default SignUp;
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -81,7 +81,9 @@ const AuthForm = ({ type }: { type: FormType }) => {
               render={({ field }) => (
                 <FormItem>
                   <div className="shad-form-item">
-                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+                    <FormLabel className="shad-form-label">
+                      Nombre completo
+                    </FormLabel>
 
                     <FormControl>
                       <Input
